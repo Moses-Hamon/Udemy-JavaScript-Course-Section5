2223ec2d1f8d7edbbf7de24e47a2078a59bfb929
Moses-Hamon <moses_hamon@bigpond.com>
2019-01-01 20:22:50 +1000

    finished lesson 66

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -30,4 +30,88 @@ console.log(peter.lastName);
 john.calcAge();
 peter.calcAge();
 mark.calcAge();
+*/
+// Object.create
+
+/*
+var age = 28;
+var obj = {
+    name: 'Moe',
+    city: 'Brisbane'
+};
+
+function change(a,b) {
+    a = 24;
+    b.city = 'Melbourne';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+console.log(test);
+*/
+
+
+var birthYear = [ 1986, 2013, 2015, 1988, 1990 ];
+
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrResult.push(fn(arr[i]));
+    }
+    return arrResult;
+}
+
+//max heart rate for age
+function maxHeartRate(element) {
+    if (element >= 18 && element <= 81) {
+        return Math.round(206.9 - (0.67 * element));
+    } else {
+        return -1;
+    }
+
+}
+
+// Determines if the age is above 18
+function isOfAge(element){
+    return element >= 18;
+}
+
+// calculates age
+function calcAge(element) {
+    return 2018 - element;
+}
+
+var ages = arrayCalc(birthYear, calcAge);
+var fullAge = arrayCalc(ages, isOfAge);
+var heartRates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAge);
+console.log(heartRates);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
