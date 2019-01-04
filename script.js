84f985a6538e5b49a692c3fca303c807f59f6430
Moses <moses_hamon@bigpond.com>
2019-01-04 12:59:02 +1000

    Lecture 69 Finished

--- script.js
+++ script.js
@@ -54,6 +54,8 @@ console.log(obj.city);
 console.log(test);
 */
 
+
+///////////////////////////////////////
 /* - Lecture 65 - Passing functions as Arguments
 
 var birthYear = [ 1986, 2013, 2015, 1988, 1990 ];
@@ -96,6 +98,8 @@ console.log(heartRates);
 
 */
 
+
+///////////////////////////////////////
 /* - Lecture 66 - Function Returning functions
 
 function interviewQuestion(job) {
@@ -146,17 +150,146 @@ game();
 
 ///////////////////////////////////////
 // Closures - Lecture 68
+ 
+/*
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementDE = retirement(65);
+var retireIceland = retirement(67);
+
+retirementDE(1986);
+retirementUS(1986);
+retireIceland(1986);
+
+*/
+
+// retirement(66)(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do');
+        }
+    }
+}
+*/
+
+/*
+function interviewQuestion(job) {
+    return function(name) {
+        switch (job) {
+            case 'designer': console.log(name + ', can you please explain what UX design is?');
+                break;
+            case 'teacher': console.log(name + ', What subject do you teach?');
+                break;
+            default: console.log('Hello ' + name + ', what do you do?');
+                break;
+        }
+    }
+}
+
+interviewQuestion('teach')('Moe');
+
+*/
+
+///////////////////////////////////////
+// Bind, Call and Apply - Lecture 69
+
+/* 
+
+//john object
+var john = {
+    name: 'john',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' 
+            + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age
+            + ' years old.' );
+        } else if (style === 'friendly') {
+            console.log('Hey! what\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age
+            + ' years old. have a nice ' + timeOfDay);
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
 
 
+john.presentation('formal', 'morning');
+john.presentation('friendly', 'afternoon');
+
+// substitutes another object to be used in the method
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// can supply the arguments to a called function (in an array) using another object
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+// binds the function to a variable.
+emily.pres = john.presentation.bind(emily, 'friendly', 'day');
+
+emily.pres();
 
 
+var birthYear = [ 1986, 2013, 2015, 1988, 1990 ];
+
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrResult.push(fn(arr[i]));
+    }
+    return arrResult;
+}
+
+//max heart rate for age
+function maxHeartRate(element) {
+    if (element >= 18 && element <= 81) {
+        return Math.round(206.9 - (0.67 * element));
+    } else {
+        return -1;
+    }
+}
+
+// calculates age
+function calcAge(element) {
+    return 2018 - element;
+}
+
+// Determines if the age is above 18
+function isOfAge(limit, element){
+    return element >= limit;
+}
 
 
+var ages = arrayCalc(birthYear, calcAge);
 
+var fullJapan = arrayCalc(ages, isOfAge.bind(this, 20));
+console.log(fullJapan);
+console.log(ages);
 
-
-
-
+*/
 
 
 
